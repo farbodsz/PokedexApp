f96332d40ffbc46c67b6f44dd5641aa34fc2b117
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-07-10 17:46:39 +0100

    Allow EncounterMethodProse to search for 'mName' with a new constructor

--- app/src/main/java/com/satsumasoftware/pokedex/framework/encounter/EncounterMethodProse.java
+++ app/src/main/java/com/satsumasoftware/pokedex/framework/encounter/EncounterMethodProse.java
@@ -1,5 +1,10 @@
 package com.satsumasoftware.pokedex.framework.encounter;
 
+import android.content.Context;
+import android.database.Cursor;
+
+import com.satsumasoftware.pokedex.db.PokeDB;
+
 public class EncounterMethodProse {
 
     private int mEncounterMethodId;
@@ -8,6 +13,21 @@ public class EncounterMethodProse {
     public EncounterMethodProse(int encounterMethodId, String name) {
         mEncounterMethodId = encounterMethodId;
         mName = name;
+    }
+
+    public EncounterMethodProse(Context context, int encounterMethodId) {
+        mEncounterMethodId = encounterMethodId;
+        PokeDB pokeDB = new PokeDB(context);
+        Cursor cursor = pokeDB.getReadableDatabase().query(
+                PokeDB.EncounterMethodProse.TABLE_NAME,
+                null,
+                PokeDB.EncounterMethodProse.COL_ENCOUNTER_METHOD_ID + "=? AND " +
+                        PokeDB.EncounterMethodProse.COL_LOCAL_LANGUAGE_ID + "=?",
+                new String[] {String.valueOf(mEncounterMethodId), "9"},
+                null, null, null);
+        cursor.moveToFirst();
+        mName = cursor.getString(cursor.getColumnIndex(PokeDB.EncounterMethodProse.COL_NAME));
+        cursor.close();
     }
 
     public int getEncounterMethodId() {
