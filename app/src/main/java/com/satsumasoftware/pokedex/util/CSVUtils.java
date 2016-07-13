f2641db78eb1429d54dd4d9db024c704c4a3955c
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-07-13 18:06:21 +0100

    Delete ExperienceDB and references to it as it is redundant

--- app/src/main/java/com/satsumasoftware/pokedex/util/CSVUtils.java
+++ app/src/main/java/com/satsumasoftware/pokedex/util/CSVUtils.java
@@ -7,6 +7,4 @@ public final class CSVUtils {
 
     public static final String CHANGELOG = "changelog.csv";
 
-    public static final String EXP_INFO_DB = "data/ExperienceDB.csv"; // TODO remove
-
 }
