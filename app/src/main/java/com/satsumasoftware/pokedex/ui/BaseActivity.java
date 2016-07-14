fb2bfa5cd6e196e05a0e6ba68fe470b5af8561e4
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-07-14 19:09:26 +0100

    Add settings as a navigation drawer item in BaseActivity
    Resolves #65

--- app/src/main/java/com/satsumasoftware/pokedex/ui/BaseActivity.java
+++ app/src/main/java/com/satsumasoftware/pokedex/ui/BaseActivity.java
@@ -63,6 +63,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected static final int NAVDRAWER_ITEM_LOCATIONDEX = R.id.navigation_item_locations;
     protected static final int NAVDRAWER_ITEM_FAVOURITES = R.id.navigation_item_favourites;
     protected static final int NAVDRAWER_ITEM_EXPERIENCE = R.id.navigation_item_experience;
+    protected static final int NAVDRAWER_ITEM_SETTINGS = R.id.navigation_item_settings;
     protected static final int NAVDRAWER_ITEM_INVALID = -1;
 
     /*
@@ -210,6 +211,11 @@ public abstract class BaseActivity extends AppCompatActivity {
                 intent = new Intent(this, ExperienceCalculatorActivity.class);
                 startActivity(intent);
                 finish();
+                break;
+            case NAVDRAWER_ITEM_SETTINGS:
+                intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                // do NOT finish
                 break;
         }
     }
