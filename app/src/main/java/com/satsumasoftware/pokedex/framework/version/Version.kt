30bf3ebbc0f486878c35c28e4c23ab495d0de4b4
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-07-23 23:45:48 +0100

    Create a data class for versions

---
+++ app/src/main/java/com/satsumasoftware/pokedex/framework/version/Version.kt
@@ -0,0 +1,32 @@
+package com.satsumasoftware.pokedex.framework.version
+
+import android.content.Context
+import android.database.Cursor
+import com.satsumasoftware.pokedex.db.PokeDB
+
+data class Version(val id: Int, val versionGroupId: Int) {
+
+    private var name: String? = null
+
+    constructor(cursor: Cursor) : this(
+            cursor.getInt(cursor.getColumnIndex(PokeDB.Versions.COL_ID)),
+            cursor.getInt(cursor.getColumnIndex(PokeDB.Versions.COL_VERSION_GROUP_ID)))
+
+    fun fetchName(context: Context): String {
+        name?.let { return it }
+        val pokeDB = PokeDB(context)
+        val cursor = pokeDB.readableDatabase.query(
+                PokeDB.VersionNames.TABLE_NAME,
+                null,
+                "${PokeDB.VersionNames.COL_VERSION_ID}=? AND " +
+                        "${PokeDB.VersionNames.COL_LOCAL_LANGUAGE_ID}=?",
+                arrayOf(id.toString(), 9.toString()),
+                null, null, null)
+        cursor.moveToFirst()
+        if (cursor.count == 0) throw NullPointerException("no name found")
+        val name = cursor.getString(cursor.getColumnIndex(PokeDB.VersionNames.COL_NAME))
+        cursor.close()
+        return name
+    }
+
+}
