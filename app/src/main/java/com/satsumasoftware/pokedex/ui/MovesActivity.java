e381538dff5153b6f2d7d2b3305a7668963bfbe0
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-07-29 22:04:18 +0100

    Display scrollbar indicators for moves sorted by id and by name

--- app/src/main/java/com/satsumasoftware/pokedex/ui/MovesActivity.java
+++ app/src/main/java/com/satsumasoftware/pokedex/ui/MovesActivity.java
@@ -33,6 +33,8 @@ import com.satsumasoftware.pokedex.util.AlertUtils;
 import com.satsumasoftware.pokedex.util.DataUtilsKt;
 import com.satsumasoftware.pokedex.util.Flavours;
 import com.satsumasoftware.pokedex.util.PrefUtils;
+import com.turingtechnologies.materialscrollbar.AlphabetIndicator;
+import com.turingtechnologies.materialscrollbar.CustomIndicator;
 import com.turingtechnologies.materialscrollbar.DragScrollBar;
 
 import java.util.ArrayList;
@@ -62,6 +64,8 @@ public class MovesActivity extends BaseActivity implements FilterListItemVGAdapt
     private View mRootLayout;
     private View mNoResults;
 
+    private DragScrollBar mScrollBar;
+
     private String mFilterSelectionName = "",
             mFilterSelectionType = "",
             mFilterSelectionGen = "";
@@ -87,7 +91,7 @@ public class MovesActivity extends BaseActivity implements FilterListItemVGAdapt
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
 
-        DragScrollBar scrollBar = new DragScrollBar(this, mRecyclerView, false);
+        mScrollBar = new DragScrollBar(this, mRecyclerView, false);
 
         mSortByName = PrefUtils.sortMovesAlphabetically(this);
 
@@ -113,7 +117,7 @@ public class MovesActivity extends BaseActivity implements FilterListItemVGAdapt
 
         });
         final ArrayList<MiniMove> itemsFinal = items;
-        MoveDexAdapter adapter = new MoveDexAdapter(itemsFinal);
+        MoveDexAdapter adapter = new MoveDexAdapter(this, itemsFinal);
         adapter.setOnRowClickListener(new MoveDexAdapter.OnRowClickListener() {
             @Override
             public void onRowClick(View view, int position) {
@@ -123,6 +127,10 @@ public class MovesActivity extends BaseActivity implements FilterListItemVGAdapt
             }
         });
         mRecyclerView.setAdapter(adapter);
+
+        mScrollBar.removeIndicator()
+                .addIndicator(mSortByName ?
+                        new AlphabetIndicator(this) : new CustomIndicator(this), true);
     }
 
 
