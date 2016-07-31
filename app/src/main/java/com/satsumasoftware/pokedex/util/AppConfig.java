037ca0c7e8eb54833f818842bb26f07845bbbc5e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-07-31 17:24:11 +0100

    Remove unused constants

--- app/src/main/java/com/satsumasoftware/pokedex/util/AppConfig.java
+++ app/src/main/java/com/satsumasoftware/pokedex/util/AppConfig.java
@@ -7,20 +7,6 @@ public final class AppConfig {
 
     public static final int MAX_NATIONAL_ID = 721;
 
-    public static final int GAME_VERSION_RED_BLUE = 1;
-    public static final int GAME_VERSION_YELLOW = 2;
-    public static final int GAME_VERSION_GOLD_SILVER = 3;
-    public static final int GAME_VERSION_CRYSTAL = 4;
-    public static final int GAME_VERSION_RUBY_SAPPHIRE = 5;
-    public static final int GAME_VERSION_EMERALD = 6;
-    public static final int GAME_VERSION_FIRERED_LEAFGREEN = 7;
-    public static final int GAME_VERSION_DIAMOND_PEARL = 8;
-    public static final int GAME_VERSION_PLATINUM = 9;
-    public static final int GAME_VERSION_HEARTGOLD_SOULSILVER = 10;
-    public static final int GAME_VERSION_BLACK_WHITE = 11;
-    public static final int GAME_VERSION_BLACK2_WHITE2 = 12;
-    public static final int GAME_VERSION_X_Y = 13;
-
     public static final int LEARN_METHOD_LEVEL_UP = 1;
     public static final int LEARN_METHOD_EGG = 2;
     public static final int LEARN_METHOD_TUTOR = 3;
