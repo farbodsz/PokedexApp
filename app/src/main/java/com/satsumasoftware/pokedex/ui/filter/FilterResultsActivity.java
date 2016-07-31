50a3178bc6252c60a38ecf5563de4275f34a2612
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-07-31 17:23:38 +0100

    Correct versionGroupId when filtering learnset data

--- app/src/main/java/com/satsumasoftware/pokedex/ui/filter/FilterResultsActivity.java
+++ app/src/main/java/com/satsumasoftware/pokedex/ui/filter/FilterResultsActivity.java
@@ -284,7 +284,9 @@ public class FilterResultsActivity extends AppCompatActivity {
         PokeDB pokeDB = new PokeDB(this);
         SQLiteDatabase db = pokeDB.getReadableDatabase();
 
-        String gameFilter = "(" + PokeDB.PokemonMoves.COL_VERSION_GROUP_ID + "=" + AppConfig.GAME_VERSION_X_Y + ")";
+        final int versionGroupId = 16;  // TODO: add option to change version group
+
+        String gameFilter = "(" + PokeDB.PokemonMoves.COL_VERSION_GROUP_ID + "=" + versionGroupId + ")";
         String learnFilter = "(" + PokeDB.PokemonMoves.COL_POKEMON_MOVE_METHOD_ID + "=" + AppConfig.LEARN_METHOD_LEVEL_UP +
                 " OR " + PokeDB.PokemonMoves.COL_POKEMON_MOVE_METHOD_ID + "=" + AppConfig.LEARN_METHOD_MACHINE +
                 " OR " + PokeDB.PokemonMoves.COL_POKEMON_MOVE_METHOD_ID + "=" + AppConfig.LEARN_METHOD_TUTOR + ")";
