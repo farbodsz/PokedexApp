6cf92888298734095d26ddc56403210ba34e72fe
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-09 21:03:07 +0100

    Use my own layout instead of relying on predefined Android layouts
    Resolves #79

--- app/src/main/java/com/satsumasoftware/pokedex/ui/SettingsActivity.java
+++ app/src/main/java/com/satsumasoftware/pokedex/ui/SettingsActivity.java
@@ -2,11 +2,9 @@ package com.satsumasoftware.pokedex.ui;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.widget.LinearLayout;
+import android.view.View;
 
 import com.satsumasoftware.pokedex.R;
 
@@ -15,26 +13,22 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_frame);
 
-        // Display the fragment as the main content.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_close_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.content, new SettingsFragment())
                 .commit();
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // http://stackoverflow.com/questions/26564400/creating-a-preference-screen-with-support-v21-toolbar
-        LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
-        AppBarLayout appBar = (AppBarLayout) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
-        root.addView(appBar, 0); // insert at top
-        setSupportActionBar((Toolbar) root.findViewById(R.id.toolbar));
-
-        assert getSupportActionBar() != null;
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public static class SettingsFragment extends PreferenceFragment {
@@ -45,4 +39,5 @@ public class SettingsActivity extends AppCompatActivity {
             addPreferencesFromResource(R.xml.preferences_fragment);
         }
     }
+
 }
