13722eebb7bed1b7cae6f8cbc72fcb24ebb18ba8
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-09-07 02:20:46 +0100

    Rewrite Ability in kotlin

--- app/src/main/java/com/satsumasoftware/pokedex/framework/ability/BaseAbility.java
+++ app/src/main/java/com/satsumasoftware/pokedex/framework/ability/BaseAbility.java
@@ -2,20 +2,9 @@ package com.satsumasoftware.pokedex.framework.ability;
 
 import com.satsumasoftware.pokedex.db.AbilitiesDBHelper;
 
-public class BaseAbility {
+public final class BaseAbility {
 
     public static final String[] DB_COLUMNS =
             {AbilitiesDBHelper.COL_ID, AbilitiesDBHelper.COL_NAME};
-
-    protected int mId;
-    protected String mName;
-
-    public int getId() {
-        return mId;
-    }
-
-    public String getName() {
-        return mName;
-    }
 
 }
