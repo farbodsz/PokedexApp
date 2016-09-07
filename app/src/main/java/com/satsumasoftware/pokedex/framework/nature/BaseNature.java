e5ba9d8aac7cfefad1e52e5591c81f018c3e6f61
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-09-07 02:38:53 +0100

    Rewrite Nature in kotlin

--- app/src/main/java/com/satsumasoftware/pokedex/framework/nature/BaseNature.java
+++ app/src/main/java/com/satsumasoftware/pokedex/framework/nature/BaseNature.java
@@ -2,19 +2,9 @@ package com.satsumasoftware.pokedex.framework.nature;
 
 import com.satsumasoftware.pokedex.db.NaturesDBHelper;
 
-public class BaseNature {
+public final class BaseNature {
 
     public static final String[] DB_COLUMNS =
             {NaturesDBHelper.COL_ID, NaturesDBHelper.COL_NAME};
 
-    protected int mId;
-    protected String mName;
-
-    public int getId() {
-        return mId;
-    }
-
-    public String getName() {
-        return mName;
-    }
 }
