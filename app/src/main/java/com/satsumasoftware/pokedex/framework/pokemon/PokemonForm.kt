64ae79b4d91207ab8e670aaab29e48fecc8c1753
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-09-07 14:01:35 +0100

    Rewrite PokemonForm in kotlin

---
+++ app/src/main/java/com/satsumasoftware/pokedex/framework/pokemon/PokemonForm.kt
@@ -0,0 +1,59 @@
+package com.satsumasoftware.pokedex.framework.pokemon
+
+import java.util.*
+import android.os.Parcel
+import android.os.Parcelable
+
+import android.widget.ImageView
+import com.satsumasoftware.pokedex.util.ActionUtils
+import com.satsumasoftware.pokedex.util.formatPokemonId
+
+class PokemonForm(val id: Int, val speciesId: Int, val formId: Int, val name: String,
+                  val formName: String, val combinedName: String, val nationalDexNumber: Int,
+                  val primaryTypeId: Int, val isDefault: Boolean, val isFormDefault: Boolean,
+                  val isFormMega: Boolean) : Parcelable {
+
+    constructor(source: Parcel) : this(
+            source.readInt(),
+            source.readInt(),
+            source.readInt(),
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readInt(),
+            source.readInt(),
+            1.equals(source.readInt()),
+            1.equals(source.readInt()),
+            1.equals(source.readInt()))
+
+    fun setPokemonImage(imageView: ImageView) {
+        ActionUtils.setPokemonImage(id,
+                formatPokemonId(speciesId),
+                name,
+                isFormMega,
+                imageView)
+    }
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeInt(id)
+        dest?.writeInt(speciesId)
+        dest?.writeInt(formId)
+        dest?.writeString(name)
+        dest?.writeString(formName)
+        dest?.writeString(combinedName)
+        dest?.writeInt(nationalDexNumber)
+        dest?.writeInt(primaryTypeId)
+        dest?.writeInt((if (isDefault) 1 else 0))
+        dest?.writeInt((if (isFormDefault) 1 else 0))
+        dest?.writeInt((if (isFormMega) 1 else 0))
+    }
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<PokemonForm> = object : Parcelable.Creator<PokemonForm> {
+            override fun createFromParcel(source: Parcel): PokemonForm = PokemonForm(source)
+            override fun newArray(size: Int): Array<PokemonForm?> = arrayOfNulls(size)
+        }
+    }
+}
