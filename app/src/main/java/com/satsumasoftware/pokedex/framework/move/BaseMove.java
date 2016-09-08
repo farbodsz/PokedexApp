3ce123ff17a5f1f2a4b0ef216c52bc555c8b1da1
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-09-08 15:21:02 +0100

    Rewrite Move in kotlin

--- app/src/main/java/com/satsumasoftware/pokedex/framework/move/BaseMove.java
+++ app/src/main/java/com/satsumasoftware/pokedex/framework/move/BaseMove.java
@@ -6,15 +6,4 @@ public class BaseMove {
 
     public static final String[] DB_COLUMNS =
             {MovesDBHelper.COL_ID, MovesDBHelper.COL_NAME};
-
-    protected int mId;
-    protected String mName;
-
-    public int getId() {
-        return mId;
-    }
-
-    public String getName() {
-        return mName;
-    }
 }
