4f13e4536f508ec9624596ef466455b9543404e6
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-09-11 16:42:40 +0100

    Add a @JvmStatic create function in LocationArea

--- app/src/main/java/com/satsumasoftware/pokedex/framework/location/LocationArea.kt
+++ app/src/main/java/com/satsumasoftware/pokedex/framework/location/LocationArea.kt
@@ -1,11 +1,31 @@
 package com.satsumasoftware.pokedex.framework.location
 
 import android.content.Context
+import com.satsumasoftware.pokedex.db.LocationAreasDBHelper
 import com.satsumasoftware.pokedex.db.PokeDB
 import com.satsumasoftware.pokedex.framework.encounter.Encounter
 import java.util.*
 
 class LocationArea(val id: Int, val locationId: Int, val name: String) {
+
+    companion object {
+        @JvmStatic
+        fun create(context: Context, id: Int): LocationArea {
+            val db = LocationAreasDBHelper.getInstance(context)
+            val cursor = db.readableDatabase.query(
+                    LocationAreasDBHelper.TABLE_NAME,
+                    null,
+                    LocationAreasDBHelper.COL_ID + "=?",
+                    arrayOf(id.toString()),
+                    null, null, null)
+            cursor.moveToFirst()
+            val locationId = cursor.getInt(cursor.getColumnIndex(
+                    LocationAreasDBHelper.COL_LOCATION_ID))
+            val name = cursor.getString(cursor.getColumnIndex(LocationAreasDBHelper.COL_NAME))
+            cursor.close()
+            return LocationArea(id, locationId, name)
+        }
+    }
 
     fun findEncounterGameVersions(context: Context): ArrayList<Int> {
         val pokeDB = PokeDB.getInstance(context)
