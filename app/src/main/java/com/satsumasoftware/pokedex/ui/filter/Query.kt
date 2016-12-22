de535bce1f35e6bedbe5f2e02e379bffde60f9aa
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-12-22 23:51:58 +0000

    Create a Query class with an inner Builder to make filtering easier

---
+++ app/src/main/java/com/satsumasoftware/pokedex/ui/filter/Query.kt
@@ -0,0 +1,30 @@
+package com.satsumasoftware.pokedex.ui.filter
+
+/**
+ * The query object is used to filter results when searching through the database
+ */
+class Query(val filter: Filter) {
+
+    class Builder {
+
+        internal var filter: Filter? = null
+
+        fun addFilter(filter: Filter): Builder {
+            this.filter = if (this.filter != null) {
+                Filters.and(this.filter!!, filter)
+            } else {
+                filter
+            }
+
+            return this
+        }
+
+        fun build(): Query {
+            if (filter == null) {
+                throw IllegalStateException("you must add at least one filter to the query builder")
+            }
+
+            return Query(filter!!)
+        }
+    }
+}
