0f8f6d7f960ee472f86a7461f5eaa23f132851f2
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-12-25 15:34:51 +0000

    Optimize imports

--- app/src/main/java/com/satsumasoftware/pokedex/ui/AbilitiesActivity.java
+++ app/src/main/java/com/satsumasoftware/pokedex/ui/AbilitiesActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
--- app/src/main/java/com/satsumasoftware/pokedex/ui/filter/SearchResultsActivity.java
+++ app/src/main/java/com/satsumasoftware/pokedex/ui/filter/SearchResultsActivity.java
@@ -3,7 +3,6 @@ package com.satsumasoftware.pokedex.ui.filter;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
